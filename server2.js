6b82246f5b9f7e943c46affe60b0f307068939a9
elliotmb <elliotisnotinasecretsociety@gmail.com>
2014-05-02 21:34:24 -0700

    server

--- server2.js
+++
@@ -1,6 +0,0 @@
-// own modules
-var FileServer = require('./module/fileServer');
-var ChatServer = require('./module/bingoServer');
-
-var fileServer = new FileServer(843);
-var sakeServer = new sakeServer(fileServer, {'log level': 1});
